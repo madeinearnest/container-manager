47c2a2b805865ca30dcb6f72a8cee6d43665c175
Harry Spink <harry@madeinearnest.uk>
2018-10-24 12:09:49 +0100

    Change branding to ECM

--- slaveInstall.sh
+++ slaveInstall.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-echo "                     #####  ######  "
-echo "#    #   ##   ##### #     # #     # "
-echo "##   #  #  #    #   #       #     # "
-echo "# #  # #    #   #   #       ######  "
-echo "#  # # ######   #   #       #       "
-echo "#   ## #    #   #   #     # #       "
-echo "#    # #    #   #    #####  #       "
-echo " "
+echo " .----------------.  .----------------.  .----------------."
+echo "| .--------------. || .--------------. || .--------------. |"
+echo "| |  _________   | || |     ______   | || | ____    ____ | |"
+echo "| | |_   ___  |  | || |   .' ___  |  | || ||_   \  /   _|| |"
+echo "| |   | |_  \_|  | || |  / .'   \_|  | || |  |   \/   |  | |"
+echo "| |   |  _|  _   | || |  | |         | || |  | |\  /| |  | |"
+echo "| |  _| |___/ |  | || |  \ `.___.'\  | || | _| |_\/_| |_ | |"
+echo "| | |_________|  | || |   `._____.'  | || ||_____||_____|| |"
+echo "| |              | || |              | || |              | |"
+echo "| '--------------' || '--------------' || '--------------' |"
+echo " '----------------'  '----------------'  '----------------'"
+
 echo "-------> Installer v0.01            "
-echo "----> Installing natCP v0.5 slave   "
+echo "----> Installing ECM v0.01 slave   "
 echo "-> You have 10 seconds to cancel.   "
 echo " "
 
@@ -21,8 +25,8 @@ sleep 10
 echo "-> Installing packages"
 yum install -y git expr bc &> /dev/null 
 echo "-> Downloading natCP files..."
-cd /tmp && git clone https://github.com/FlamesRunner/natCP
-mv /tmp/natCP/slave /tmp/slave && rm -rf /tmp/natCP
+cd /tmp && git clone https://github.com/madeinearnest/container-manager
+mv /tmp/container-manager /tmp/slave
 chmod 700 /tmp/slave/*
 mv /tmp/slave/* /sbin
 echo "-> Installing OpenVZ kernel"
